47c0b37c7f28bde975358228c6fc3966696111ac
Vilas Karnake <vilas.karnake@elevate.law>
2025-09-30 16:20:57 +0530

    upload all files

---
+++ myApp/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1 className='text-green-500 text-4xl font-bold'>My First Project</h1>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <div className="bg-red-500 text-white p-4">
+  Tailwind is working!
+</div>
+    </>
+  )
+}
+
+export default App
